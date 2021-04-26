d7ef12b4aef702d6fdecf484a899844f1cf14c46
uni0912 <cwyun0912@gmail.com>
2021-04-26 11:32:50 +0900

    InputClass

--- KopoLand_project/kopoland/ConstValueClass.java
+++ KopoLand_project/kopoland/ConstValueClass.java
@@ -1,6 +1,7 @@
 package kopoland;
 
 public class ConstValueClass {
+	//주간권/야간권에 따른 나이별 가격
 	final static int BABY_PRICE = 0;
 	final static int ADULT_DAY_PRICE = 56000, ADULT_NIGHT_PRICE = 46000;
 	final static int TEEN_DAY_PRICE = 47000, TEEN_NIGHT_PRICE = 40000;
@@ -14,7 +15,7 @@ public class ConstValueClass {
 	final static int MIN_TEEN = 13, MAX_TEEN = 18;
 	final static int MIN_ADULT = 19, MAX_ADULT = 64;
 	final static int MIN_OLD = 65;
-	//할인율
+	//우대사항에 따른 할인율
 	final static double DISABLE_DISCOUNT_RATE = 0.6, MERIT_DISCOUNT_RATE = 0.5,
 			MULTICHILD_DISCOUNT_RATE = 0.8, PREGNANT_DISCOUNT_RATE = 0.85;
 }
--- KopoLand_project/kopoland/InputClass.java
+++ KopoLand_project/kopoland/InputClass.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputClass {
 	Scanner scanner = null;
 	
-	InputClass() {
+	InputClass() { //생성자 입력
 		scanner = new Scanner(System.in);
 	}
 	
@@ -13,11 +13,11 @@ public class InputClass {
 		scanner.close();
 	}
 
-	public int inputTicketSelect() {
+	public int inputTicketSelect() { //권종 선택
 		int ticketSelect = 0;
 		System.out.println("권종을 선택하세요");
-		System.out.println("1.주간권");
-		System.out.println("2.야간권");
+		System.out.println("1. 주간권");
+		System.out.println("2. 야간권");
 		try {
 			ticketSelect = scanner.nextInt();
 		} catch (Exception e) {
@@ -26,7 +26,7 @@ public class InputClass {
 		return ticketSelect;
 	}
 	
-	public int inputCustomerIDNumber() {
+	public int inputCustomerIDNumber() { //고객정보입력
 		int customerIDNumber = 0;
 		System.out.println("생년월일을 입력하세요");
 		try {
@@ -35,5 +35,32 @@ public class InputClass {
 			// TODO: handle exception
 		}
 		return customerIDNumber;
+	}
+	
+	public int inputTicketCount() { //주문개수입력
+		int ticketCount = 0;
+		System.out.println("몇개를 주문하시겠습니까?(최대10개)");
+		try {
+			ticketCount = scanner.nextInt();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return ticketCount;
+	}
+	
+	public int inputDiscountSlect() { //우대사항 입력
+		int discountSelect = 0;
+		System.out.println("우대사항을 선택하세요.");
+		System.out.println("1. 없음(나이 우대는 자동처리");
+		System.out.println("2. 장애인");
+		System.out.println("3. 국가유공자");
+		System.out.println("4. 다자녀");
+		System.out.println("5. 임산부");
+		try {
+			discountSelect = scanner.nextInt();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return discountSelect;
 	}
 }
